c749c34da1ca8f7c560399b0429a6118a4cbae23
JosueRomeroQuesada <josuerq2003@gmail.com>
2023-03-09 20:19:48 -0600

    Inicio del proyecto de Desarrollo web

---
+++ src/test/java/com/proyecto_final_g05/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_final_g05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
