67a83e694ec923301207ef2bfa3955adad442fe0
emers <emers@DESKTOP-8RLOVHM>
2023-03-13 23:11:40 -0600

    Avance 1. Desarrollo de la plantilla, para el menu de navegación, y el pie de la pagina, además de controller, y establecer el puerto 80

---
+++ src/main/java/com/proyecto_final_g05/controller/InicioController.java
@@ -0,0 +1,19 @@
+package com.proyecto_final_g05.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author emers
+ */
+@Controller
+public class InicioController {
+    
+    @GetMapping("/")
+    public String inicio(Model model){      
+        return "Inicio";
+    }
+    
+}
