2e32529256edf3047ed84f9a9c3ba9eab338fd15
emers <emers@DESKTOP-8RLOVHM>
2023-04-24 16:11:06 -0600

    Modificacion del header para solucionar problema grafico

--- src/main/java/com/proyecto_final_g05/controller/CategoriaController.java
+++ src/main/java/com/proyecto_final_g05/controller/CategoriaController.java
@@ -1,8 +1,4 @@
-package com.proyecto_final_g05.controller;
-
-/*import com.proyecto_final_g05.domain.Categoria;*/
 import com.proyecto_final_g05.domain.Categoria;
-import com.proyecto_final_g05.service.ArticuloService;
 import com.proyecto_final_g05.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,24 +6,48 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+ 
 /**
- *
- * @author emers
- */
+*
+* @author emers
+*/
 @Controller
-@RequestMapping("/cartucheras")
+@RequestMapping("/categoria")
 public class CategoriaController {
-
+ 
     @Autowired
-    private ArticuloService articuloService;
-
+    private CategoriaService categoriaService;
+ 
     @GetMapping("/listado")
-    public String inicio(Model model) {
-        var articulos = articuloService.getArticulos(false);
-        model.addAttribute("articulos", articulos);
-        model.addAttribute("totalArticulos", articulos.size());
-        return "cartucheras/listado";
+    public String inicio(Model model){  
+        var categorias=categoriaService.getCategorias(false);
+        model.addAttribute("categorias",categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
+    }
+ 
+    @GetMapping("/eliminar/{idCategoria}")
+    public String eliminaCategoria(Categoria categoria){
+        categoriaService.deleteCategoria(categoria);
+        return "redirect:/categoria/listado";
+    }
+ 
+     @GetMapping("/modificar/{idCategoria}")
+    public String modificaCategoria(Categoria categoria,Model model){
+        categoria=categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }
+ 
+ 
+    @GetMapping("/nuevo")
+    public String nuevoCategoria(Categoria categoria){
+        return "/categoria/modifica";
+    }
+ 
+    @PostMapping("/guardar")
+    public String guardarCategoria(Categoria categoria){
+        categoriaService.saveCategoria(categoria);
+        return "redirect:/categoria/listado";
     }
 }
- m
